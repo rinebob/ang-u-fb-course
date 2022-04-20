b5a89774ed7b8a9b59a31aa750722343452a8996
rinebob <rinebob111185@gmail.com>
2022-04-19 21:11:19 -0700

    Added UI elements for Home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,8 @@ import {Observable, of} from 'rxjs';
 import {catchError, map} from 'rxjs/operators';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {Router} from '@angular/router';
+import { CoursesService } from '../services/courses.service';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 
 @Component({
@@ -18,12 +20,20 @@ export class HomeComponent implements OnInit {
     advancedCourses$: Observable<Course[]>;
 
     constructor(
-      private router: Router) {
+      private router: Router,
+      private coursesService: CoursesService,
+      ) {
 
     }
 
     ngOnInit() {
+      this.loadCourses();
 
+    }
+
+    loadCourses() {
+      this.beginnersCourses$ = this.coursesService.loadCoursesByCategory('BEGINNER');
+      this.advancedCourses$ = this.coursesService.loadCoursesByCategory('ADVANCED');
     }
 
 }
